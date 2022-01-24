d22ef6d17ed31485ae57a6417c57ddda9fee3d87
hykare <78385964+hykare@users.noreply.github.com>
2022-01-24 01:39:49 +0100

    add interactive game loop and a method to change gameboard state

--- ttt.rb
+++ ttt.rb
@@ -11,7 +11,23 @@ class Board
     end
   end
 
+  def update(row, col, mark)
+    @state[row - 1][col - 1] = mark
+  end
 end
 
 game_board = Board.new
-game_board.draw
+
+condition = true
+while condition
+  system 'clear'
+  game_board.draw
+  puts 'make a move'
+  print 'row: '
+  move_row = gets.chomp.to_i
+  print 'column: '
+  move_col = gets.chomp.to_i
+  game_board.update(move_row, move_col, 'x')
+
+end
+
