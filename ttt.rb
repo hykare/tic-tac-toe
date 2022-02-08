72fb79b5bbdeb7192ed8ff771121283d7435b740
hykare <78385964+hykare@users.noreply.github.com>
2022-02-08 14:55:52 +0100

    check for win/tie/undetermined match result

--- ttt.rb
+++ ttt.rb
@@ -18,6 +18,28 @@ class Board
     col = position[1].tr('abc', '012').to_i
     @state[row][col] = player.mark
   end
+
+  def check_match_result
+    # horizontal
+    @state.each do |row|
+      return 'o_won' if row.all? { |mark| mark == 'o' }
+      return 'x_won' if row.all? { |mark| mark == 'x' }
+    end
+    # vertical
+    (0..2).each do |i|
+      return 'o_won' if @state.all? { |row| row[i] == 'o' }
+      return 'x_won' if @state.all? { |row| row[i] == 'x' }
+    end
+    # diagonals
+    return 'o_won' if @state[0][0] == 'o' && @state[1][1] == 'o' && @state[2][2] == 'o'
+    return 'o_won' if @state[0][2] == 'o' && @state[1][1] == 'o' && @state[2][0] == 'o'
+    return 'x_won' if @state[0][0] == 'x' && @state[1][1] == 'x' && @state[2][2] == 'x'
+    return 'x_won' if @state[0][2] == 'x' && @state[1][1] == 'x' && @state[2][0] == 'x'
+
+    return 'undetermined' if @state.any? { |row| row.any? { |mark| mark == ' ' } }
+
+    'tie'
+  end
 end
 
 class CurrentPlayer
@@ -49,13 +71,15 @@ current_player = CurrentPlayer.new
 condition = true
 while condition
   system 'clear'
+  puts "match result: #{game_board.check_match_result}"
   game_board.draw
   position = get_move_position
   game_board.update(position, current_player)
   current_player.switch
+
 end
 
 # TODO
-# check for win
-# check if board full - tie
 # reject taken positions
+# fix markers so they're not strings
+# fix match results so they're not strings
